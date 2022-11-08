f831bed09090649830c2f842bd03c92ffda7e038
Paul BERNARD <pbernard@inflexsys.com>
2022-11-08 15:52:18 +0100

    Ajout du plugin appointment hour booking

---
+++ wp-content/plugins/appointment-hour-booking/cp-widget.inc.php
@@ -0,0 +1,6 @@
+<?php
+  // to get the widget go to https://apphourbooking.dwbooster.com/download
+  
+  if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+  
+?>
---
+++ wp-content/themes/xtrembarbershop/pages/booking.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Template name: Reservation
+*/
+include(__DIR__ . "/header.php"); ?>
+
+<div class="container">
+    <?php echo do_shortcode("[CP_APP_HOUR_BOOKING id='1']
+"); ?>
+</div>
+
+<?php include(__DIR__ . "/footer.php"); ?>
